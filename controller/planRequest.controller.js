dac3933ee6344676d9ae27a5ddc36aaad2adb1e2
Praveen Patidar <pravinpatidar718@gmail.com>
2025-03-05 11:04:53 +0530

    upt

--- controller/planRequest.controller.js
+++ controller/planRequest.controller.js
@@ -1,38 +1,55 @@
 import { PlanRequest } from "../model/planRequest.model.js";
+import { Subscription } from "../model/subscription.model.js";
+import { User } from "../model/user.model.js";
 
-export const addPlan=async(req,res,next)=>{
+export const addPlan = async (req, res, next) => {
     try {
-        if(req.file){
-            req.body.paymentImg=req.file.filename;
+        if (req.file) {
+            req.body.paymentImg = req.file.filename;
         }
-        const plan=await PlanRequest.create(req.body);
-        return plan ?res.status(200).json({message:"Request Added",status:true}):res.status(404).json({message:"Something Went Wrong",status:false})
+        const plan = await PlanRequest.create(req.body);
+        return plan ? res.status(200).json({ message: "Request Added", status: true }) : res.status(404).json({ message: "Something Went Wrong", status: false })
     } catch (error) {
         console.log(error)
-        res.status(500).json({message:"Internal Server Error",error:error.message,status:false})
+        res.status(500).json({ message: "Internal Server Error", error: error.message, status: false })
     }
 }
 
-export const viewPlan=async(req,res,next)=>{
+export const viewPlan = async (req, res, next) => {
     try {
-        const plan=await PlanRequest.find();
-        return plan.length>0?res.status(200).json({message:"Data Found",plan,status:true}):res.status(404).json({message:"Not Found",status:false})
+        const plan = await PlanRequest.find();
+        return plan.length > 0 ? res.status(200).json({ message: "Data Found", plan, status: true }) : res.status(404).json({ message: "Not Found", status: false })
     } catch (error) {
         console.log(error)
-        res.status(500).json({message:"Internal Server Error",error:error.message,status:false})    
+        res.status(500).json({ message: "Internal Server Error", error: error.message, status: false })
     }
 }
 
-export const updatePlan=async(req,res,next)=>{
+export const updatePlan = async (req, res, next) => {
     try {
-        const {id}=req.params;
-        const plan=await PlanRequest.findById(id);
-        if(!plan){
-            return res.status(404).json({message:"Not Found",status:false})
+        const { id } = req.params;
+        const existingPlan = await PlanRequest.findById(id);
+        if (!existingPlan) {
+            return res.status(404).json({ message: "Not Found", status: false })
         }
-
+        if (req.body.status === "Approved") {
+            const user = await User.findById(existingPlan.superAdmin);
+            if (user) {
+                const sub = await Subscription.findById({ _id: req.body.plan })
+                if (sub) {
+                    // const { _id, ...subWithoutId } = sub.toObject();
+                    const date = new Date();
+                    user.planStart = date;
+                    user.planEnd = new Date(date.getTime() + (sub.days * 24 * 60 * 60 * 1000));
+                    user.billAmount = sub.subscriptionCost
+                    user.userAllotted = sub.noOfUser
+                }
+            }
+            await user.save();
+        }
+        res.status(200).json({ message: "Status Updated", status: true })
     } catch (error) {
         console.log(error)
-        res.status(500).json({message:"Internal Server Error",error:error.message,status:false})    
+        res.status(500).json({ message: "Internal Server Error", error: error.message, status: false })
     }
 }
