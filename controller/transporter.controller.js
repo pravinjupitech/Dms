cab6a3d620e5039118de7c8b817a0172988b26d6
Praveen Patidar <pravinpatidar718@gmail.com>
2025-03-22 17:26:06 +0530

    updt

--- controller/transporter.controller.js
+++ controller/transporter.controller.js
@@ -125,8 +125,7 @@ export const saveExcelFile = async (req, res) => {
             document[database] = req.params.database
             if (document.database) {
                 const role = await Role.findOne({ id: document.rolename, database: document.database })
-                console.log("role",role)
-                console.log("document.rolename",document.rolename,"document.database",document.database)
+               
                 if (!role) {
                     roles.push(document.id)
                 } else {
