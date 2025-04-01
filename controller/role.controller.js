157e5d47e644c18f993c091c67be2eb26b0b97da
Praveen Patidar <pravinpatidar718@gmail.com>
2025-04-01 15:16:30 +0530

    updt

--- controller/role.controller.js
+++ controller/role.controller.js
@@ -81,8 +81,8 @@ export const updatedRole = async (req, res, next) => {
 
 export const saveRole = async (req, res, next) => {
     try {
+        console.log("req.body",req.body)
         for (let roleData of req.body.Roles) {
-            console.log("roleData",roleData)
             const existingRole = await Role.findOne({ roleName: roleData.role.roleName, database: roleData.role.database });
             if (existingRole) {
                 // console.log(`Role with name ${roleData.role.roleName} already exists.`);
