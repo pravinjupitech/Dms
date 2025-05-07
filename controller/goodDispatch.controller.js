32d5a87499e3ca451bc95c23b92423cc3cc065a1
Praveen Patidar <pravinpatidar718@gmail.com>
2025-05-07 12:18:36 +0530

    updt

--- controller/goodDispatch.controller.js
+++ controller/goodDispatch.controller.js
@@ -336,9 +336,10 @@ export const ViewOtp = async (req, res) => {
         return res.status(500).json({ error: 'Internal Server Error', status: false });
     }
 };
+
 export const ViewWarehouseByOrder = async (req, res, next) => {
     try {
-        const order = await CreateOrder.find({ "orderItems.warehouse": req.params.id,status: { $ne: "Deactive" } }).populate({ path: "orderItems.productId", model: "product" }).populate({ path: "partyId", model: "customer" }).populate({ path: "userId", model: "user" }).populate({ path: "AssignDeliveryBoy", model: "user" })
+        const order = await CreateOrder.find({ "orderItems.warehouse": req.params.id, status: "Billing", status: { $ne: "Deactive" } }).populate({ path: "orderItems.productId", model: "product" }).populate({ path: "partyId", model: "customer" }).populate({ path: "userId", model: "user" }).populate({ path: "AssignDeliveryBoy", model: "user" })
         if (order.length === 0) {
             return res.status(404).json({ message: "warehouse stock not found", status: false })
         }
