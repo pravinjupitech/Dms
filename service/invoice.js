ed09f670c21da4ea8cadf35d5e9180585bb7fa1f
Praveen Patidar <pravinpatidar718@gmail.com>
2025-08-25 15:10:22 +0530

    up

--- service/invoice.js
+++ service/invoice.js
@@ -41,14 +41,11 @@ export const generateInvoice = async (database) => {
 };
 export const generateOrderNo = async (database) => {
     const companyDetail = await CompanyDetails.findOne({ database: database });
-    console.log("companyDetails",companyDetail)
     const invoice = companyDetail.orderNo + 1;
     let orderNO = invoice.toString().padStart(3, '0')
-    console.log("orderNo",orderNO)
     const generatedOrderNo = `${"ORD"}${orderNO}`;
     companyDetail.orderNo = invoice;
     await companyDetail.save()
-console.log("generatedorderno",generatedOrderNo)
     return generatedOrderNo;
 };
 export const warehouseNo = async (database) => {
