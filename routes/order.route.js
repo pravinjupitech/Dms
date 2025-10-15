0b011177908def0aa3d38eefce34fdb78c994c0b
Praveen Patidar <pravinpatidar718@gmail.com>
2025-10-15 12:36:31 +0530

    upd

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path"
 import multer from "multer";
-import { CheckPartyPayment, DebitorCalculate, DispatchOrderCancelFromWarehouse, InvoiceIdFrom, OrdertoBilling, OrdertoDispatch, PartyPurchaseqty, ProductWiseSalesReport, SalesOrderCalculate, SalesOrderList, ViewOrderHistoryForPartySalesPerson, checkPartyOrderLimit, createOrder, createOrderHistory, createOrderHistoryById, createOrderHistoryByPartyId, createOrderHistoryByUserId, createOrderWithInvoice, deleteSalesOrder, deletedSalesOrder, deletedSalesOrderMultiple, invoicePartySend, updateCNDetails, updateCreateOrder, updateCreateOrderStatus, updateOrderArn} from "../controller/order.controller.js";
+import { CheckPartyPayment, DebitorCalculate, DispatchOrderCancelFromWarehouse, InvoiceIdFrom, OrdertoBilling, OrdertoDispatch, PartyPurchaseqty, ProductWiseSalesReport, SalesOrderCalculate, SalesOrderList, ViewOrderHistoryForPartySalesPerson, checkPartyOrderLimit, createOrder, createOrderHistory, createOrderHistoryById, createOrderHistoryByPartyId, createOrderHistoryByUserId, createOrderWithInvoice, deleteSalesOrder, deletedSalesOrder, deletedSalesOrderMultiple, hsnWiseSaleReportB2B, hsnWiseSaleReportB2C, invoicePartySend, updateCNDetails, updateCreateOrder, updateCreateOrderStatus, updateOrderArn} from "../controller/order.controller.js";
 
 const router = express.Router();
 const storage = multer.diskStorage({
@@ -46,4 +46,6 @@ router.post("/send-invoice",upload.single("invoice"),invoicePartySend)
 router.put("/update-arn/:id",updateOrderArn)
 router.put("/update-cndetails/:id",upload.single("CNImage"),updateCNDetails)
 router.get("/view-invoice-data/:database/:invoiceId",InvoiceIdFrom)
+router.get("/hsn-wise-sale-reportb2b/:database",hsnWiseSaleReportB2B)
+router.get("/hsn-wise-sale-reportb2c/:database",hsnWiseSaleReportB2C)
 export default router;
