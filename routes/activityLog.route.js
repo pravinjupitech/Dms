58a439c2d1a1b36aaf077567c811cd828348558e
Praveen Patidar <pravinpatidar718@gmail.com>
2025-11-13 16:00:22 +0530

    upd

--- routes/activityLog.route.js
+++ routes/activityLog.route.js
@@ -1,8 +1,10 @@
 import express from "express"
-import { AddLog, updateLogTime, viewLogs } from "../controller/activityLog.controller.js";
+import { AddLog, deleteAllLogs, deleteLog, updateLogTime, viewLogs } from "../controller/activityLog.controller.js";
 const router=express.Router();
 
 router.post("/save-log",AddLog)
 router.post("/update-logOutTime",updateLogTime)
 router.get("/view-logs/:database",viewLogs)
+router.delete("/delete-all",deleteAllLogs)
+router.delete("/delete-logs/:id",deleteLog)
 export default router;
