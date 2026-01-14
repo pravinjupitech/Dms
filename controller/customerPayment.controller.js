6ebef04e47535fc6a61446879cc47c55dbc54490
Praveen Patidar <pravinpatidar718@gmail.com>
2026-01-14 15:02:27 +0530

    upd

---
+++ controller/customerPayment.controller.js
@@ -0,0 +1,58 @@
+import { CustomerPayment } from "../model/customerPayment.model.js";
+import { Receipt } from "../model/receipt.model.js";
+
+export const SaveCustomerPayment = async (req, res) => {
+    try {
+        const payload = {
+            ...req.body,
+            type: "receipt"
+        };
+
+        const customerPayment = await CustomerPayment.create(payload);
+        const receipt = await Receipt.create(payload);
+
+        return res.status(201).json({
+            message: "Customer payment and receipt created successfully",
+            status: true,
+            data: {
+                customerPayment,
+                receipt
+            }
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            status: false
+        });
+    }
+};
+
+export const viewCustomerPayment = async (req, res, next) => {
+    try {
+        const { partyId } = req.params;
+        const customerPayment = await CustomerPayment.find({ partyId: partyId });
+        return customerPayment.length > 0 ? res.status(200).json({ message: "Data Found", customerPayment, status: true }) : res.status(400).json({ message: "Not Found", status: false })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            status: false
+        });
+    }
+}
+
+export const viewByIdCustomer = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const customerPayment = await CustomerPayment.findById(id);
+        return customerPayment ? res.status(200).json({ message: "Data Found", customerPayment, status: true }) : res.status(400).json({ message: "Not Found", status: false })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            status: false
+        });
+    }
+}
---
+++ routes/customerPayment.route.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { SaveCustomerPayment, viewByIdCustomer, viewCustomerPayment } from "../controller/customerPayment.controller.js";
+const router = express.Router();
+
+router.post("/save-customer-payment",SaveCustomerPayment)
+router.get("/view-customer-payment/:partyId",viewCustomerPayment)
+router.get("/view-by-customer-payment/:id",viewByIdCustomer)
