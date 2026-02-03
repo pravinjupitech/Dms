2d9a45814484327290a7e40c9c3acc6da5c54ed8
Praveen Patidar <pravinpatidar718@gmail.com>
2026-02-03 14:38:27 +0530

    3-feb

--- routes/combinedTarget.route.js
+++ routes/combinedTarget.route.js
@@ -1,18 +1,19 @@
+// 
+
+// routes/customerTargetRoutes.js
 import express from "express";
 import {
-  saveCombinedTarget,
-  listCombinedTargets,
-  updateCombinedTarget,
-  deleteCombinedTarget,
-  combinedTargetUpload,
-} from "../controller/combinedTarget.controller.js";
+  setCustomerTarget,
+  getSalesPersonTarget,
+  getSalesManagerTarget,
+  getHierarchyByDatabase,
+} from "../controller/combinedTarget.controller.js"
 
 const router = express.Router();
 
-router.post("/", combinedTargetUpload, saveCombinedTarget);
-router.post("/save", combinedTargetUpload, saveCombinedTarget);
-router.get("/", listCombinedTargets);
-router.put("/:id", updateCombinedTarget);
-router.delete("/:id", deleteCombinedTarget);
+router.post("/customer-target", setCustomerTarget);
+router.post("/show-target",getHierarchyByDatabase)
+router.post("/sales-person-target", getSalesPersonTarget);
+router.get("/sales-manager-target/:salesManagerId/:financialYear", getSalesManagerTarget);
 
 export default router;
