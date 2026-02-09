dd722670f54a480b30ec2d411106365962a62cd9
Praveen Patidar <pravinpatidar718@gmail.com>
2026-02-09 20:02:06 +0530

    06-feb

--- routes/companyTarget.route.js
+++ routes/companyTarget.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCompanyTarget, getSalesManagerTarget, saveCompanyTarget } from "../controller/companyTarget.controller.js";
+import { deleteCompanyTargetById, getCompanyTarget, getSalesManagerTarget, saveCompanyTarget, updateCompanyTarget } from "../controller/companyTarget.controller.js";
 const router=express.Router();
 
 router.post("/save-company-target",saveCompanyTarget)
 router.get("/get-company-target/:fyear/:database",getCompanyTarget)
 router.get("/get-salesmanager-target/:fyear/:salesManagerId",getSalesManagerTarget)
-
+router.delete("/delete-comapany-target/:id",deleteCompanyTargetById)
+router.put("/update-target",updateCompanyTarget)
 export default router;
